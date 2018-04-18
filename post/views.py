3ffbac29458aa2979b5af41843d6fb785c861a99
wxy72 <18227109996@163.com>
2018-04-18 18:09:58 +0800

    meiyou suo jin BUG le bao bot defind cuowu

--- post/views.py
+++ post/views.py
@@ -1,32 +1,30 @@
-from django.shortcuts import render,redirect
+from urllib import request
+
+from django.shortcuts import render, redirect
 
 from post.models import Post
 
+
 # Create your views here.
 def create_post(requst):
-	if request.method == 'POST':
+    if request.method == 'POST':
         title = request.POST.get('title')
         content = request.POST.get('content')
-        
-        post = Post.objects.create(title=title , content=content)
+        post = Post.objects.create(title=title, content=content)
         return redirect('/read/?post_id=%s' % post.id)
 
     return render(request, 'create_post.html', {})
- 
+
 
 def post_list(requst):
-	pass
+    pass
+
 
 def edit_post(requst):
-	pass
+    pass
+
 
 def read_post(requst):
-	post_id = int(request.GET.get('post_id'))
-    post = Post.objects.get(id=post_id)
-    return render(request, 'read_post.html',{'post':post})
-
-
-
-
-
-
+    post_id = int(request.GET.get('post_id'))
+    post = Post.objects.get(id='post_id')
+    return render(request, 'read_post.html', {'post': post})
