07e350cc12219c4ac626cef3f412bca91f95c74e
wxy72 <18227109996@163.com>
2018-04-18 17:31:50 +0800

    chongxin tijiao view

--- demo/urls.py
+++ demo/urls.py
@@ -13,13 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url,include
 from django.contrib import admin
 
 urlpatterns = [
     # url(r'^admin/', admin.site.urls),
-    url(r'^post/list/', post_view.post_list),
-    url(r'^post/create/', post_view.create_post),
-    url(r'^post/edit/', post_view.edit_post),
-    url(r'^post/read/', post_view.read_post),
+ 	url(r'post/',include('post.urls')),
 ]
--- post/views.py
+++ post/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+
+from post.models import Post
 
 # Create your views here.
+def create_post(requst):
+	if request.method == 'POST':
+        title = request.POST.get('title')
+        content = request.POST.get('content')
+        
+        post = Post.objects.create(title=title, content=content)
+        return redirect('/read/?post_id=%s' % post.id)
+
+    return render(request, 'create_post.html', {})
+ 
+
+def post_list(requst):
+	pass
+
+def edit_post(requst):
+	pass
+
+def read_post(requst):
+	pass
+
+
+
+
+
